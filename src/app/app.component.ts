f2888167ac5f18de3d6e714f574202c594b92ad4
timc986 <timothychan986@hotmail.com>
2021-01-14 12:31:24 +0000

    close sidenav when on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MediaObserver } from '@angular/flex-layout';
+
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public isOpen = true;
+  private mediaSubscription;
+
+  constructor(public mediaObserver: MediaObserver) {
+    this.mediaSubscription = mediaObserver.asObservable().subscribe(changes => {
+      this.isOpen = (changes[0].mqAlias !== 'xs');
+      console.log(changes.map(c => c.mqAlias));
+    });
+  }
+
+  ngOnDestroy() {
+    this.mediaSubscription.unsubscribe();
+  }
+
+  toggleSideNav() {
+    this.isOpen = !this.isOpen;
+  }
+
 }
